d2d85591f72a7c3fc676ba056db86ca361ea0e8d
Stacy Morang <smorang@Stacys-Mac-Pro.localdomain>
2026-02-16 18:47:03 -0500

    Updates for GNE38

--- app/lib/pennsic.rb
+++ app/lib/pennsic.rb
@@ -27,7 +27,7 @@ class Pennsic
 
   # these are the dates we allow classes - nothing sunday, basically
   def self.class_dates_raw
-    (Date.new(2025,7,09)..Date.new(2025,7,11)).to_a
+    (Date.new(2026,7,09)..Date.new(2026,7,11)).to_a
   end
 
   def self.class_dates
